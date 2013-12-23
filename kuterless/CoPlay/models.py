6e80da4646d541e0a8d4af909a6750c2ecf02bd8
TzahiM <tzahimanmobile@gmail.com>
2013-12-23 11:44:02 +0200

    model not tested

--- kuterless/CoPlay/models.py
+++ kuterless/CoPlay/models.py
@@ -4,11 +4,22 @@ from django.db import models
 # Create your models here.
 
 class Attendant(models.Model):
-    name = models.CharField( unique = True, db_index=True, max_length=20, validators=[MaxLengthValidator])
+    name = models.CharField( unique = True, db_index=True, max_length=20)
 
     def __unicode__(self):
         return self.name
     
+
+class Discussion(models.Model):
+    Owner = models.ForeignKey(Attendant)
+    Title = models.CharField( unique = True, max_length=200)
+    Description = models.CharField(blank = True, null = True, max_length=600, validators=[MaxLengthValidator])
+    create_date = models.DateTimeField('date created', auto_now_add=True )
+    update_date = models.DateTimeField('last-modifie', auto_now =True )
+    
+    def __unicode__(self):
+        return self.id
+
 
 
 class LikeLevel(object):
@@ -29,6 +40,7 @@ class LikeLevel(object):
     
     
 class Decision(models.Model):
+    discussion = models.ForeignKey(Discussion)
     TextBody = models.CharField( max_length=200, validators=[MaxLengthValidator],editable =False)
     create_date = models.DateTimeField('date created', auto_now_add=True )
 
@@ -37,8 +49,8 @@ class Decision(models.Model):
     
     
 class Vote(models.Model):
-    decision  = models.ForeignKey(Decision, editable =False )
-    Voater = models.ForeignKey(Attendant, editable =False)
+    decision  = models.ForeignKey(Decision )
+    Voater = models.ForeignKey(Attendant)
     create_date = models.DateTimeField('date created', auto_now_add=True )
     update_date = models.DateTimeField('last-modifie', auto_now =True )
     Value = models.IntegerField( default=LikeLevel.NOT_DEFINED, choices =LikeLevel.level, null=True,
@@ -49,14 +61,18 @@ class Vote(models.Model):
     
 
 class Action(models.Model):
-    responsible = models.OneToOneField(Attendant, verbose_name="Responsible")
-    Witnesses = models.ManyToManyField( Attendant, related_name='w+')
-    GoalDescription = models.CharField(  max_length=200, validators=[MaxLengthValidator])
+    discussion = models.ForeignKey(Discussion)
+    responsible = models.ForeignKey(Attendant)
+    GoalDescription = models.CharField(  max_length=200)
     target_date = models.DateTimeField('Should be completed untill')
     closing_date = models.DateTimeField('Achived at',blank = True,  null = True)
     StatusDescription = models.TextField(blank=True, null = True)
     def __unicode__(self):
         return self.id
+
+class Witness(models.Model):
+    responsible = models.ForeignKey(Attendant)
+    action = models.ForeignKey(Action)
 
 class Response(models.Model):
     RESPONSE_TYPES  = (
@@ -66,7 +82,7 @@ class Response(models.Model):
         ('A','Advise'     ),
     )
     
-    
+    discussion = models.ForeignKey(Discussion)
     attendant = models.ForeignKey(Attendant, verbose_name="Attendant",  db_index=True)
     ResponseType = models.CharField(max_length=1, choices=RESPONSE_TYPES, db_index=True)
     TextBody = models.CharField(  max_length=600, validators=[MaxLengthValidator] )
@@ -76,18 +92,6 @@ class Response(models.Model):
         return self.id
 
 
-class Discussion(models.Model):
-    Owner = models.ForeignKey(Attendant)
-    Title = models.CharField( unique = True, max_length=200, validators=[MaxLengthValidator])
-    Description = models.CharField(blank = True, null = True, max_length=600, validators=[MaxLengthValidator])
-    create_date = models.DateTimeField('date created', auto_now_add=True )
-    update_date = models.DateTimeField('last-modifie', auto_now =True )
-    Responses   = models.ManyToManyField(Response, related_name='r+', blank = True, null = True)
-    Actions   = models.ManyToManyField(Action, related_name='a+', blank = True, null = True)
-    Decisions   = models.ManyToManyField(Decision, related_name='d+',blank = True, null = True)
-    
-    def __unicode__(self):
-        return self.id
         
         
             
