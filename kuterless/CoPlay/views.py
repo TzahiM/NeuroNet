b6239148142e1e813d1bf90eac1e6ce3d16a5b86
TzahiM <tzahimanmobile@gmail.com>
2013-12-29 14:26:58 +0200

    url scheme for coplay tested

--- kuterless/CoPlay/views.py
+++ kuterless/CoPlay/views.py
@@ -1,12 +1,15 @@
 from coplay.models import Discussion
+from django.http.response import HttpResponse
 from django.views import generic
 from django.views.generic.edit import CreateView
 
 # Create your views here.
 def root(request):
+    return HttpResponse("root")
     
 
 class IndexView(generic.ListView):
+    model = Discussion
     template_name = 'coplay/index.html'
     context_object_name = 'latest_discussion_list'
 
@@ -20,19 +23,27 @@ class DiscussionAddView(CreateView):
     model = Discussion
 
 def update_discussion(request, pk):
+    return HttpResponse("update_discussion" + pk)
     
     
-def add_decision(request, pk):    
+def add_decision(request, pk):
+    return HttpResponse("add_decision" + pk)
+        
     
 def vote(request, pk):    
+    return HttpResponse("vote" + pk)
 
 def add_task(request, pk):    
+    return HttpResponse("add_task" + pk)
     
 def task_details(request, pk):    
+    return HttpResponse("task_details"  + pk)
     
     
 def update_task_description(request, pk, new_description):  
-    
+    return HttpResponse("update_task_description " + new_description)
+   
 def close_task(request, pk):      
+    return HttpResponse("close_task" + pk)
 
 
