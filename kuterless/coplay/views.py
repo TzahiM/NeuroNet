f8d90d1f0d909a48474016bafbd2606ab00b6a67
TzahiM <tzahimanmobile@gmail.com>
2014-05-19 14:37:43 +0300

    debug

--- kuterless/coplay/views.py
+++ kuterless/coplay/views.py
@@ -823,15 +823,9 @@ class CreateFeedbackView(CreateView):
         form.instance.discussion = self.discussion
         form.instance.user = self.request.user
         resp = super(CreateFeedbackView, self).form_valid(form)  
-        subject_text = get_user_fullname_or_username(self.request.user) 
-        + ' ' 
-        + u"הוסיף/ה"
-        + ' ' 
-        + form.instance.get_feedbabk_type_name()
-        + ' ' 
-        + form.instance.discussion.title
+        subject_text = get_user_fullname_or_username(self.request.user)+ ' '+ u"הוסיף/ה"+ ' '+ form.instance.get_feedbabk_type_name()+ u"הוסיף/ה" + form.instance.discussion.title
         
-        details = subject_text + ':\n\n"' +  form.instance.content + '"\n'
+        details = subject_text + ':\n\n"' +  form.instance.content + '\n'
                                                             
                                                             
                                                             
