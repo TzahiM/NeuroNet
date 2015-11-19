c52bd92d8a0ef156d4fb5d8ffdee5b9e85370e4a
TzahiM <tzahimanmobile@gmail.com>
2015-11-19 22:08:45 +0200

    change default to DEBUG = False

--- kuterless/kuterless/settings.py
+++ kuterless/kuterless/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '+i4mx76+pi29$50gdbxh%111@it_hz=(bw39@o16uru=_-pxa+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
