9321803e767213c086d499c181f43ce13121a314
TzahiM <tzahimanmobile@gmail.com>
2016-05-16 22:20:10 +0300

    fix a buf in task retrieval

--- kuterless/coplay/views.py
+++ kuterless/coplay/views.py
@@ -147,7 +147,7 @@ def discussion_details(request, pk):
     list_tasks_open = discussion.task_set.all().order_by("target_date").filter(status = Task.STARTED)
     
 #     list_tasks_closed_and_aborted = discussion.task_set.all().exclude(status = Task.MISSED).filter(final_state = True).order_by("-closed_at")
-    list_tasks_closed_and_aborted = discussion.task_set.all().exclude(status = Task.MISSED).order_by("-closed_at")
+    list_tasks_closed_and_aborted = discussion.task_set.all().exclude(status = Task.MISSED).order_by("-updated_at")
 
     list_tasks = list(list_tasks_open) + list( list_tasks_closed_and_aborted)
     
