93d26aa86f5449e48e3b4cae84d1521f6e887aaf
Tzahim <tzahimanmobile@gmail.com>
2018-01-07 19:06:13 +0200

    direct to http://hey.pbme.co/pHBfqJ

--- kuterless/kuterless/urls.py
+++ kuterless/kuterless/urls.py
@@ -13,7 +13,7 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 admin.autodiscover()
 
 def redirect_neuronet(request):
-    return HttpResponseRedirect('https://www.facebook.com/NeuroNetWorkshops/')
+    return HttpResponseRedirect('http://hey.pbme.co/pHBfqJ')
 
 urlpatterns = patterns('',
     # Examples:
